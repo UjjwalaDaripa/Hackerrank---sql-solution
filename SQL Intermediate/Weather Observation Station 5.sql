3f4d4527e68e83cb285051aad0479866cdf9152d
UjjwalaDaripa <ujjwala.daripa@gmail.com>
2024-08-08 01:20:35 +0530

    Update Weather Observation Station 5.sql

--- SQL Intermediate/Weather Observation Station 5.sql
+++ SQL Intermediate/Weather Observation Station 5.sql
@@ -1,1 +1,8 @@
+with cte (city,len,rank) as(
+Select  city, len(city) , rank() over (partition by len(city) order by city) as rank
+from station
+where len(city)=(select min(len(city)) from station)
+or len(city)=(select max(len(city)) from station))
 
+select city,len from cte
+where rank =1
